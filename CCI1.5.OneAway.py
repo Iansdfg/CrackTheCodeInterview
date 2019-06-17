530e854ec4482d4de0499e554f3ff1a9306966f3
Iansdfg <47009085+Iansdfg@users.noreply.github.com>
2019-06-17 12:13:12 -0700

    Update CCI1.5.OneAway.py

--- CCI1.5.OneAway.py
+++ CCI1.5.OneAway.py
@@ -2,47 +2,27 @@
 class Solution(object):
     def OneAway (self, s1, s2):
         if s1 == s2: return True
-
-        m = len(s1)
-        n = len(s2)
-
-        if abs(m - n) > 1:
-            return False
-
-        count = 0  # Count of isEditDistanceOne
-
-        i = 0
-        j = 0
+        m, n = len(s1), len(s2)
+        if abs(m - n) > 1: return False
+        count, i, j = 0, 0, 0
         while i < m and j < n:
-            # If current characters dont match
             if s1[i] != s2[j]:
                 if count == 1:
                     return False
-
-                    # If length of one string is
-                # more, then only possible edit
-                # is to remove a character
                 if m > n:
                     i += 1
                 elif m < n:
                     j += 1
-                else:  # If lengths of both strings is same
+                else:
                     i += 1
                     j += 1
-
-                # Increment count of edits
                 count += 1
-
-            else:  # if current characters match
+            else:
                 i += 1
                 j += 1
-
-        # if last character is extra in any string
         if i < m or j < n:
             count += 1
-
         return count == 1
-
 
 def execute():
     sol = Solution()
