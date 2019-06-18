da0203ef25fcdc308a4c481c68b08f403f1dbe80
Iansdfg <47009085+Iansdfg@users.noreply.github.com>
2019-06-17 22:25:58 -0700

    Create CCI1.7.Rotate Matrix.py

---
+++ CCI1.7.Rotate Matrix.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        if not matrix or not matrix[0]:
+            return []
+        n = len(matrix)
+        for layer in range(n//2):
+            first, last = layer, n-1-layer
+            for i in range(first, last):
+                offset = i - first
+                top = matrix[first][i]
+                matrix[first][i] = matrix[last-offset][first]
+                matrix[last-offset][first] = matrix[last][last-offset]
+                matrix[last][last-offset] = matrix[i][last]
+                matrix[i][last] = top
+        
+                
+                
+            
+            
+        
+        
